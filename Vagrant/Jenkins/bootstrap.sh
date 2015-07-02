3446291085511fab7ca59f60969ec6dc31804910
Thomas Wiedemann <thomas.wiedemann84@gmail.com>
2015-07-02 21:15:33 +0200

    added jenkins-vagrant files

---
+++ Vagrant/Jenkins/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get install -y -f jenkins
+service jenkins start
